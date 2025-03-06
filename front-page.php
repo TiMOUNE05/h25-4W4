0546565b2e8672b30bc1ecc6795085700a0ff39b
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-03-06 11:00:18 -0500

    ajout du template part hero

--- front-page.php
+++ front-page.php
@@ -9,35 +9,7 @@
 
 <?php get_header() ?>
 <h1>front-page.php</h1>
-    <section class="hero">
-        <div class="hero__contenu global">
-            <h1 class="hero__titre">
-                <?php echo bloginfo('name') ?>
-            </h1>
-            <p class="hero__description">
-                <?php echo bloginfo('description') ?>
-            </p>
-            <p class="hero__courriel">
-                info@cmaisonneuve.qc.ca
-            </p>
-            <p class="hero__addresse">
-                3800, rue Sherbrooke, Montreal
-            </p>
-            <p class="hero__numero">
-                514-254-7131
-            </p>
-            <button class="hero__bouton">
-                s'inscrire
-            </button>
-            <div class="hero__icone-app">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-            </div>
-        </div>
-    </section>
-    
+<?php get_template_part( 'gabarits/hero' ); ?>
     <section class="form__formulaire">
         <div class="formulaire">
             <form>
