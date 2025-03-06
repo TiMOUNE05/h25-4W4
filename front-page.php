c3bad52d76b8dc3fa1ee37bb80f97d1fc0805dc6
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-03-06 12:24:57 -0500

    modification de la couleur du lien et l'affiche de la galerie

--- front-page.php
+++ front-page.php
@@ -8,7 +8,6 @@
 
 
 <?php get_header() ?>
-<h1>front-page.php</h1>
 <?php get_template_part( 'gabarits/hero' ); ?>
     <section class="form__formulaire">
         <div class="formulaire">
@@ -33,46 +32,11 @@
             </form>
         </div>
     </section>
-    <section class="galerie">
-        <h2>Nos destinations favorites</h2>
-        <div class="galerie__destinations">
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage1.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage2.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage3.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage4.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage5.jpg' ?>"class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage6.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage7.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage8.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage9.jpg' ?>" class="galerie__img">
-            </figure>
-            <figure class="galerie__fig">
-                <img src="<?php echo get_template_directory_uri() . '/images/paysage10.jpg' ?>"class="galerie__img">
-            </figure>
-        </div>   
-    </section>
- 
-    <section class="populaire">
-        <div class="boiteflex global">
+
+   <section class="populaire">
+        <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); 
-            if (in_category("category"))  {
+            if (in_category("Galerie"))  {
                 the_content() ;
             } else {    ?>
                 <?php get_template_part( 'gabarits/carte' ); ?>
--- readme.md
+++ readme.md
@@ -1,7 +1,5 @@
-# Exercice 1
-## Cours: 4W4-Conception d'interface et développement Web
-
-Créer une page d'accueil en utilisant le standard BEM et Sass pour reproduire le contenu de maquette.
-Auteur: Yohan Jacques
-### Github-page: https://timoune05.github.io/h25-4W4/
+# Auteur: Yohan Jacques
+## Titre: Site de club de voyage
+Créer un site de club de voyage selon une maquette
+### Lien du site: https://gftnth00.mywhc.ca/4w4_34/
 
