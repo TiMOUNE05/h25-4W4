60d2f226e5f6023b3e4e0eaf1ccbe1609963cba8
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-03-13 10:27:19 -0400

    ajout de la fonction du changement de couleur de l'icone dans le customizer

--- functions.php
+++ functions.php
@@ -58,13 +58,13 @@ $wp_customize->add_control('footer_mission', array(
 //////////////////////////////////////////couleur du texte de la zone hero//////////////////////////////////////////
 
 
-$wp_customize->add_setting('hero_couleur', array(
+$wp_customize->add_setting('hero_icone', array(
   'default' => '',
   'sanitize_callback' => 'esc_url_raw',
 ));
 
-$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_couleur', array(
-  'label' => __('couleur du texte', 'theme_31w'),
+$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_icone', array(
+  'label' => __('Couleur du texte', 'theme_31w'),
   'section' => 'hero_section',
 )));
 
--- gabarits/hero.php
+++ gabarits/hero.php
@@ -1,5 +1,13 @@
-<?php $hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); ?>
-<?php $hero_background = get_theme_mod('hero_background', 'Default Title'); ?>
+<?php 
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_background = get_theme_mod('hero_background', 'Default Title');
+$couleur = substr(get_theme_mod('hero_icone', '#fff'),1); 
+?>
+<style>
+    .hero_couleur{
+         color: <?php echo $couleur ?>
+    }
+</style>
 <section class="hero" style= "background-image: url(<?php echo $hero_background?>); background-repeat: no-repeat;">
         <div class="hero__contenu global">
             <h1 class="hero__titre">
@@ -21,10 +29,10 @@
                 s'inscrire
             </button>
             <div class="hero__icone-app">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo $couleur ?>" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo $couleur ?>" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?php echo $couleur ?>" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?php echo $couleur ?>" width="20" height="20">
             </div>
             <p> auteur: <?php echo $hero_auteur?></p>
         </div>
