249a705bc7f870727c8ab6876ba3cc7c3b705733
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-03-18 11:27:00 -0400

    ajout d'un template part formulaire

---
+++ gabarits/formulaire.php
@@ -0,0 +1,23 @@
+<section class="form__formulaire">
+        <div class="formulaire">
+            <form>
+                <div class="form__reponse">
+                    <label for="nom">Nom:</label><br>
+                    <input type="text" class="formulaire__input" name="nom" placeholder="Écrivez votre nom"><br>
+                </div>
+                <div class="form__reponse">
+                    <label for="prenom">Prénom:</label><br>
+                    <input type="text" class="formulaire__input" name="prenom"placeholder="Écrivez votre prénom"><br>
+                </div>
+                <div class="form__reponse">
+                    <label for="courriel">Courriel:</label><br>
+                    <input type="text" class="formulaire__input" name="courriel" placeholder="Écrivez votre courriel"><br>
+                </div>
+                <div class="form__reponse">
+                    <label for="telephone">Téléphone:</label><br>
+                    <input type="text" class="formulaire__input" name="telephone"placeholder="Écrivez votre téléphone" ><br>
+                </div>
+                    <button class="formulaire__button">S'inscrire</button>                
+            </form>
+        </div>
+    </section>
