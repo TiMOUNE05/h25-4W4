96bc52b5294780b41ba673969dbbd29ddf65a99d
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-03-25 10:30:00 -0400

    s9c1 ajout des icones dans la page 404

--- 404.php
+++ 404.php
@@ -9,4 +9,5 @@ $erreur_titre = get_theme_mod('erreur_titre', 'Default Title');
     <p class="erreur_message">
             <?php echo $erreur_message?>
     </p>
+    <?php get_template_part( 'gabarits/icone' ); ?>
 <?php get_footer(); ?>
