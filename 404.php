538fc70c05c110ccbb1a03b06c30b57ea91c887f
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-04-01 10:49:43 -0400

    ajour des liens de carte dans la page 404

--- 404.php
+++ 404.php
@@ -20,11 +20,22 @@ $erreur_background = get_theme_mod('erreur_background', 'Default Title');
     </div>
 
     <div class="erreur_boutonAccueil">
-    <a href="index.php">
-    <button>Retour à la page d'accueil</button>
-    </a>
+        <a href="index.php">
+        <button>Retour à la page d'accueil</button>
+        </a>
     </div>
+
+    <div class="erreur_boutonCarte"> 
+    <?php if (have_posts()) : while (have_posts()) : the_post(); 
+        if (in_category("populaire")) : ?>
+            <?php get_template_part( 'gabarits/carteErreur' ); ?>
+        <?php endif;
+    endwhile; endif; ?>
+</div>
+
+
     
+
     <div class="erreur_recherche">
     <?php get_search_form();?>
     </div>
---
+++ gabarits/carteErreur.php
@@ -0,0 +1,3 @@
+<div class="erreur_carte">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+</div>
