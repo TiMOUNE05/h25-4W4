cba034e5ae62ab82b150d610b31f5a2e94129d3e
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-04-03 11:58:32 -0400

    ajout de la liste de la categorie destination

--- js/destination.js
+++ js/destination.js
@@ -1,23 +1,36 @@
-(function(){
+(function() {
     console.log("vive Javascript");
 
-    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+    let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
     const domaine = window.location.href;
-    const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+    let apiUrl = `${domaine}/wp-json/wp/v2/posts?categories=${categoryId}`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const destinationList = document.querySelector('.destination__list');
-            data.forEach(article => {
-                const articleElement = document.createElement('div');
-                articleElement.innerHTML = `
-                    <h3>${article.title.rendered}</h3>
-                    <div>${article.excerpt.rendered}</div>
-                    <a href="${article.link}">Lire plus</a>
-                `;
-                destinationList .appendChild(articleElement);
-            });
-        })
-        .catch(error => console.error('Erreur lors de la récupération des articles:', error));
-})()
+    const categorie__ul__li = document.querySelectorAll(".categorie__ul__li");
+    console.log("categorie__ul__li.length", categorie__ul__li.length);
+
+    categorie__ul__li.forEach(li => {
+        li.addEventListener("click", function() {
+            console.log(li.dataset.id);
+            categoryId = li.dataset.id;
+            apiUrl = `${domaine}/wp-json/wp/v2/posts?categories=${categoryId}`;
+
+            // Refaire la requête avec le nouvel apiUrl après le clic
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const destinationList = document.querySelector('.destination__list');
+                    destinationList.innerHTML = ''; // Vider la liste avant d'ajouter les nouveaux articles
+                    data.forEach(article => {
+                        const articleElement = document.createElement('div');
+                        articleElement.innerHTML = `
+                            <h3>${article.title.rendered}</h3>
+                            <div>${article.excerpt.rendered}</div>
+                            <a href="${article.link}">Lire plus</a>
+                        `;
+                        destinationList.appendChild(articleElement);
+                    });
+                })
+                .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+        });
+    });
+})();
