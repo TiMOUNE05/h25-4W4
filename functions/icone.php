9f3cfae5d7c641cbe53cd4d5b2f0cbc70ae79a50
Jacques <e2257410@cmaisonneuve.qc.ca>
2025-05-06 10:13:20 -0400

    lien des icones fontionnelles

--- functions/icone.php
+++ functions/icone.php
@@ -1,17 +1,21 @@
 <?php
 
 function afficher_icones_sociaux() {
-    $couleur = get_theme_mod('hero_icone', '#fff');
-    $couleur = ltrim($couleur, '#');
-
+    $couleurIcone = substr(get_theme_mod('hero_icone', '#ffffff'), 1);
     $icones = ['facebook', 'linkedin', 'paypal', 'stackoverflow', 'github', 'wordpress'];
 
-    echo '<div class="icone_app">';
+    echo '<div class="icone_app" style="display: flex; gap: 10px;">';
     foreach ($icones as $icone) {
-        echo '<img src="https://s2.svgbox.net/social.svg?ic=' . $icone . '&color=' . $couleur . '" width="20" height="20">';
+        $lien = get_theme_mod("lien_icone_$icone");
+        if ($lien) {
+            echo '<a href="' . esc_url($lien) . '" target="_blank" rel="noopener">
+                <img src="https://s2.svgbox.net/social.svg?ic=' . esc_attr($icone) . '&color=' . esc_attr($couleurIcone) . '" width="20" height="20">
+            </a>';
+        }
     }
     echo '</div>';
 }
+
 
 
 ?>
